7a55d138d2bd8af38609047d39e5dd2f60fbe835
Jean-Philippe MAGIDO <jean-philippemagido@Jean-Philippes-iMac.local>
2019-06-03 13:10:44 +0200

    MODIF User Model, Create all attributes

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_06_03_101703) do
+ActiveRecord::Schema.define(version: 2019_06_03_110314) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -23,6 +23,16 @@ ActiveRecord::Schema.define(version: 2019_06_03_101703) do
     t.datetime "remember_created_at"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "first_name"
+    t.string "last_name"
+    t.integer "age"
+    t.string "profession"
+    t.string "nationality"
+    t.float "weight"
+    t.string "belt"
+    t.string "phone"
+    t.float "subscription"
+    t.integer "admin_rights"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
