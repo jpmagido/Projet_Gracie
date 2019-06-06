2d39b737842ebce856a4c87918912551a7e31bdb
Jean-Philippe MAGIDO <jean-philippemagido@Jean-Philippes-iMac.local>
2019-06-06 18:14:00 +0200

    MODIF Try new API for SendGrid. jpm

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -91,7 +91,7 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
-  
+
   # Change to TRUE if you actually want to send mails
-  config.action_mailer.perform_deliveries = false
+  #config.action_mailer.perform_deliveries = false
 end
