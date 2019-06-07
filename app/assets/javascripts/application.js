a7db78c34d15bc54c9b90d37bcbf41e58b70e295
Rodrigo Vallejos <vallejosrs@gmail.com>
2019-06-07 18:54:24 +0200

    removed files to make work js

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -13,11 +13,23 @@
 //= require rails-ujs
 //= require activestorage
 //= require turbolinks
-//= require agency.min
+//  <!-- Bootstrap core JavaScript -->
+//= require jquery/jquery.min
+//= require bootstrap/js/bootstrap.bundle.min
+//  <!-- Plugin JavaScript -->
+//= require jquery-easing/jquery.easing.min
+//  <!-- Custom scripts for this template -->
+
+//
+//
+//
+//
+//
+
 //= require jquery/jquery.min
 //= require bootstrap/js/bootstrap.bundle.min
 //= require jquery-easing/jquery.easing.min
 //= require jqBootstrapValidation
 //= require contact_me
-
+//= require agency.min
 //= require_tree .
