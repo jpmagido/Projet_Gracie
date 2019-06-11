846d43cfaf9469ea575b4fd960434d28a1d887f1
Jean-Philippe MAGIDO <jean-philippemagido@Jean-Philippes-iMac.local>
2019-06-11 13:19:50 +0200

    MODIF Static_lp: pricing: fix pb. jpm

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -21,3 +21,26 @@
 //= require contact_me
 
 //= require_tree .
+
+
+('.nav-tabs-dropdown').each(function(i, elm) {
+    
+    $(elm).text($(elm).next('ul').find('li.active a').text());
+    
+});
+  
+('.nav-tabs-dropdown').on('click', function(e) {
+
+    e.preventDefault();
+    
+    $(e.target).toggleClass('open').next('ul').slideToggle();
+    
+});
+
+('#nav-tabs-wrapper a[data-toggle="tab"]').on('click', function(e) {
+
+    e.preventDefault();
+    
+    $(e.target).closest('ul').hide().prev('a').removeClass('open').text($(this).text());
+      
+});
