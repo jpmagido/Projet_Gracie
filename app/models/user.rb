450f4651781de21430203194c318e017735750fc
Jean-Philippe MAGIDO <jean-philippemagido@Jean-Philippes-iMac.local>
2019-06-13 18:42:27 +0200

    MODIF all links social. jpm

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,15 +1,12 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
-  devise :database_authenticatable, :registerable,
-
-         :recoverable, :rememberable, :validatable
-
-  devise :omniauthable, omniauth_providers: [:facebook, :google_oauth2]
-
-
+  devise :database_authenticatable, :registerable, :recoverable, :rememberable, :validatable, :omniauthable, omniauth_providers: [:facebook, :google_oauth2]
 
          #before_action :authenticate_user!
+
+ validates :first_name, presence: :true
+ validates :last_name, presence: :true
 
   after_create :welcome_send
 
